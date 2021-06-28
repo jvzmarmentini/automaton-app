eb94893cd5467c91802740a04a934000089c2395
joão marmentini <joao.marmentini@edu.pucrs.br>
2021-06-28 20:07:45 -0300

    automaton def

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # automaton-app
+
+<M>=({<q0>,...,<qn>},{<s1>,...,<sn>},<ini>,{ <f0>,...,<fn>})
+Prog
+(<q0>,<s1>)=<q1>
+...
+(<qn>,<sn>)=<q0>TRABALHO FINAL - LINGUAGENS FORMAIS E AUTÔMATOS
+onde:
+< M >: nome dado ao autômato;
+< qi >: para 0 ≤ i ≤ n, com n ∈ N e n ≥ 0, representa um estado do 
+autômato;
+< si >: para 1 ≤ i ≤ n, com n ∈ N e n ≥1, representa um símbolo do 
+alfabeto da linguagem reconhecida pelo autômato;
+< ini >: indica o estado inicial do autômato, sendo que ini é um estado do 
+autômato;
+< fi >: para 0 ≤ i ≤ n, com n ∈ N e n ≥ 0, representa um estado final do 
+autômato, sendo que fi é um estado do autômato;
+(< qi >,< si >) =< qj > : descreve a função programa aplicada a um 
+estado qi e um símbolo de entrada si que leva a computação a um estado 
+qj.
+
+exemplo:
--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import math
 
-with open("automato.txt") as file:
-    lines = file.readlines()
-    for i in range(len(lines)):
-        lines[i] = lines[i].replace("\n", "")
-    lines = list(filter(None, lines))
-    print(lines)
+def readFile(file_path):
+    with open(file_path) as file:
+        lines = file.readlines()
+        for i in range(len(lines)):
+            lines[i] = lines[i].replace("\n", "")
+        lines[0] = lines[0].replace("AUTÔMATO=(", "")
+        lines[0] = lines[0].replace(")", "")
+        return list(filter(None, lines))
+
+def automaton(rfile):
+    line1 = rfile[0]
+
+rfile = readFile("automato.txt")
+print(rfile)
+automaton(rfile)
+graph = nx.Graph()
+# nx.draw(graph, with_labels=True, font_weight='bold')
+# plt.show()
