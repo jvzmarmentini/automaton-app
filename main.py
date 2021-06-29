268f159f499a67b59de907f74d6f9c78df68a146
joão marmentini <joao.marmentini@edu.pucrs.br>
2021-06-28 21:31:40 -0300

    creating basic automaton graph

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import networkx as nx
-import math
+from networkx.drawing.nx_agraph import to_agraph 
 
 def readFile(file_path):
     with open(file_path) as file:
@@ -9,13 +9,12 @@ def readFile(file_path):
             lines[i] = lines[i].replace("\n", "")
         lines[0] = lines[0].replace("AUTÔMATO=(", "")
         lines[0] = lines[0].replace(")", "")
+        lines[2] = lines[2].replace("Prog", "")
         return list(filter(None, lines))
 
-def automaton(rfile):
+def automaton(rfile, graph):
     automaton = rfile[0]
-    #print(automaton)
     automaton = automaton.split("},")
-    #print(automaton)
     temp = automaton[2].split(",{")
     automaton[2] = temp[0]
     automaton.append(temp[1])
@@ -25,17 +24,42 @@ def automaton(rfile):
         automaton[i] = automaton[i].replace("}", "")
     states = automaton[0].split(",")
     print("states: " + str(states))
+    gramatic = automaton[1].split(",")
+    print("gramatic: " + str(gramatic))
+    initial_state = automaton[2]
+    print("initial_state: " + str(initial_state))
+    final_states = automaton[3].split(",")
+    print("final_states: " + str(final_states))
+
+    for node in states:
+        graph.add_node(node)
 
     functions = []
     for func in rfile:
-        if(rfile.index(func) > 1):
+        if(rfile.index(func) > 0):
             functions.append(func)
+
+    for func in functions:
+        func = func.replace(")=", ",")
+        func = func.replace("(", "")
+        func = func.split(",")
+        left_node = func[0]
+        right_node = func[2]
+        simba = func[1]
+        graph.add_edge(left_node, right_node, simba=simba)
 
     print(functions)
 
 rfile = readFile("automato.txt")
 print(rfile)
-automaton(rfile)
-graph = nx.Graph()
-# nx.draw(graph, with_labels=True, font_weight='bold')
-# plt.show()
+graph = nx.DiGraph()
+automaton(rfile, graph)
+pos = nx.spring_layout(graph)
+print(graph.edges)
+nx.draw(graph, pos, with_labels=True, font_weight='bold')
+
+edge_labels = nx.get_edge_attributes(graph,'simba')
+print(edge_labels)
+nx.draw_networkx_edge_labels(graph, pos, edge_labels = edge_labels)
+
+plt.show()
