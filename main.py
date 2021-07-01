5337eb4fd3661c165edfa1d99ff8d541cc517f41
joão marmentini <joao.marmentini@edu.pucrs.br>
2021-06-30 21:01:23 -0300

    clean up

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 # TODO
-# * handle "empty symbol"
-# * optimize coverter
-# * write "pydoc"
-# * update and improve git docs
-# ?* write report
+# * handle "empty symbol" - Tasca
+# * update and improve git docs - Panho
 
 import copy
 import networkx as nx
@@ -19,7 +16,6 @@ class automata:
 
 
 def readFile(file_path):
-    # see regex. maybe theres a better way to process the file
     with open(file_path) as file:
         lines = file.readlines()
         for i in range(len(lines)):
@@ -38,37 +34,25 @@ def readFile(file_path):
 
 
 def nd2d_converter(rfile, nd_automata):
-    states = rfile[0][0].split(",")
-    # print("states: " + str(states))
-    gramatic = rfile[0][1].split(",")
-    # print("gramatic: " + str(gramatic))
-    initial_state = rfile[0][2]
-    # print("initial_state: " + str(initial_state))
-    final_states = rfile[0][3].split(",")
-    # print("final_states: " + str(final_states))
-
-    a1 = automata(states, gramatic, initial_state, final_states)
+    a1 = automata(
+        states=rfile[0][0].split(","),
+        gramatic=rfile[0][1].split(","),
+        initial_state=rfile[0][2],
+        final_states=rfile[0][3].split(",")
+    )
 
     for node in a1.states:
-        if(node in a1.final_states):
-            nd_automata.add_node(node, final=True)
-        else:
-            nd_automata.add_node(node, final=False)
+        nd_automata.add_node(
+            node, initial=node in a1.initial_state, final=node in a1.final_states)
 
     for func in rfile[1:]:
         nd_automata.add_edge(func[0], func[2],
                              label=func[1])
 
-    # notes from the dev: good luck trying understanding this little mf
-
-    grammy = {}
-    for sym in gramatic:
-        grammy[sym] = []
-
-    open_list = []
+    grammy = {sym: [] for sym in a1.gramatic}
+    open_list = [[a1.initial_state]]
     closed_list = []
     new_automata = {}
-    open_list.append([initial_state])
     while(len(open_list) > 0):
         # print("open_list" + str(open_list))
         # print("closed_list" + str(closed_list))
@@ -78,27 +62,21 @@ def nd2d_converter(rfile, nd_automata):
                 # print("neighbor: " + str(neighbor))
                 edges = nd_automata.get_edge_data(element, neighbor)
                 for sym in edges:
-                    if(not (neighbor in grammy[edges[sym]["label"]])):
+                    if(neighbor not in grammy[edges[sym]["label"]]):
                         grammy[edges[sym]["label"]].append(neighbor)
                     grammy[edges[sym]["label"]].sort()
             # print("grammy: " + str(grammy))
         new_automata["".join(open_list[0])] = copy.deepcopy(grammy.copy())
-        # print(new_automata)
+        # print("new_automatta: " + str(new_automata))
         # print("dict(grammy): " + str(dict(grammy)))
         closed_list.append(open_list.pop(0))
         for sym in grammy:
-            if (not grammy[sym] == []):
-                if(not ((grammy[sym] in open_list) or (grammy[sym] in closed_list))):
-                    grammy[sym].sort()
-                    open_list.append(grammy[sym])
-                grammy[sym] = []
+            if (not grammy[sym] == [] and grammy[sym] not in open_list and closed_list):
+                open_list.append(grammy[sym])
+            grammy[sym] = []
     for i, node in enumerate(new_automata):
-        if(node in a1.initial_state):  # CONFERIR
-            d_automata.add_node(node, initial=True, final=False)
-        elif(closed_list[i][len(closed_list[i])-1] in a1.final_states):
-            d_automata.add_node(node, initial=False, final=True)
-        else:
-            d_automata.add_node(node, initial=False, final=False)
+        d_automata.add_node(node, initial=node in a1.initial_state,
+                            final=closed_list[i][len(closed_list[i])-1] in a1.final_states)
 
     for node in new_automata:
         for edge in new_automata[node]:
@@ -111,14 +89,17 @@ def nd2d_converter(rfile, nd_automata):
 def wordProcessing(rfile, d_automata, input):
     initial_state = rfile[0][2]
     current = initial_state
+    temp = ""
     for letter in input:
         if (letter not in rfile[0][1]):
-            return "a palavra contem uma letra que nao esta no dicionario"
+            return "A palavra contém uma letra que não está na gramática!"
         for neighbor in d_automata.neighbors(current):
             edges = d_automata.get_edge_data(current, neighbor)
             for sym in edges:
                 if(letter == edges[sym]["label"]):
-                    current = neighbor
+                    temp = neighbor
+        current = temp
+
     for node in d_automata.nodes(data=True):
         if(node[0] == current):
             return node[1]["final"]
@@ -130,19 +111,35 @@ nd_automata = nx.MultiDiGraph()
 d_automata = nx.MultiDiGraph()
 
 nd2d_converter(rfile, nd_automata)
-print(wordProcessing(rfile, d_automata, ""))
+print(wordProcessing(rfile, d_automata, "LI"))
 
-old_names = list(d_automata.nodes())
+# old_names = list(d_automata.nodes())
 
-name_map = dict.fromkeys(old_names)
-for i in range(len(old_names)):
-    name_map[old_names[i]] = old_names[i]
-d_automata = nx.relabel_nodes(d_automata, name_map)
+# name_map = dict.fromkeys(old_names)
+mapping = dict(zip(d_automata.nodes(), "pqrstuvwxyzabcdefghijklmno"))
+d_automata = nx.relabel_nodes(d_automata, mapping)
 
+final = list(filter(lambda x: x[1]['final'] ==
+             True, nd_automata.nodes(data=True)))
+initial = list(filter(lambda x: x[1]['initial'] ==
+                      True, nd_automata.nodes(data=True)))[0][0]
+nd_automata.nodes[initial]['style'] = 'filled'
+nd_automata.nodes[initial]['fillcolor'] = 'green'
+for f in final:
+    nd_automata.nodes[f[0]]['shape'] = 'doublecircle'
 A = nx.nx_agraph.to_agraph(nd_automata)
 A.layout(prog="dot")
 A.draw("automatons/nd_automata.png")
 
+# print(d_automata.nodes(data=True))
+final = list(filter(lambda x: x[1]['final'] ==
+             True, d_automata.nodes(data=True)))
+initial = list(filter(lambda x: x[1]['initial'] ==
+                      True, d_automata.nodes(data=True)))[0][0]
+d_automata.nodes[initial]['style'] = 'filled'
+d_automata.nodes[initial]['fillcolor'] = 'green'
+for f in final:
+    d_automata.nodes[f[0]]['shape'] = 'doublecircle'
 B = nx.nx_agraph.to_agraph(d_automata)
 B.layout(prog="dot")
 B.draw("automatons/d_automata.png")
